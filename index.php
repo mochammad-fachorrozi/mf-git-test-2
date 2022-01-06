085c70986a58f7e968f598073725e6f1fa072403
mochammad-fachorrozi <mochammadfachorrozi98@gmail.com>
2022-01-06 10:30:19 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -12,6 +12,10 @@
 
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et laborum voluptas expedita alias molestias vitae!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 
 </body>
 
